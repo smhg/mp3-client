602091f9216e324e43d4c481223bdf153728b5f2
smhg <sam@strictlyphp.com>
2013-12-17 19:42:15 +0100

    grunt init

---
+++ Gruntfile.js
@@ -0,0 +1,63 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Task configuration.
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        globals: {}
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib: {
+        src: ['lib/**/*.js']
+      }
+    },
+    browserify: {
+      dev: {
+        options: {
+          debug: true
+        },
+        files: {
+          'index.js': 'lib/index.js'
+        }
+      },
+      dist: {
+        files: {
+          'index.js': 'lib/index.js'
+        }
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib_test: {
+        files: '<%= jshint.lib.src %>',
+        tasks: ['jshint:lib', 'browserify:dev']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint:lib', 'browserify:dist']);
+};
